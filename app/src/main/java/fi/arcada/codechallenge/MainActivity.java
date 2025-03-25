9dd8ffcfcfc1b4cae87c0677dd9afa6acf018c97
nathalie <enstromn@arcada.fi>
2025-03-25 17:16:08 +0200

    pushing

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,19 +1,48 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.WindowDecorActionBar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView = findViewById(R.id.hello);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener( View -> calculate());
+
     }
+
+
+    private void calculate () {
+        ArrayList<Double> list = new ArrayList<>();
+        list.add(1.0);
+        list.add(2.0);
+        list.add(3.0);
+        list.add(4.0);
+
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+        }
+        textView.setText("Sum: " + sum);
+
+
+    }
+
 }
